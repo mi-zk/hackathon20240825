af7a8b43e2e356fa76baaa30f334dfc91ce59946
hareshino <puyopuyochan6201@gmail.com>
2024-08-25 01:50:03 +0900

    first commit

---
+++ audio_generation.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import tempfile
+from utils.prompt_making import make_prompt
+from utils.generation import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+
+def generate_audio_from_text(audio_prompt_path, transcript, text_prompt, count):
+    # 与えられた文字起こしを使用する
+    make_prompt(name="hoge", audio_prompt_path=audio_prompt_path, transcript=transcript)
+    
+    # モデルをダウンロードしてロードする
+    preload_models()
+    
+    # 音声を生成する
+    audio_array = generate_audio(text_prompt)
+    
+    # 生成された音声をファイルに書き込む
+    # write_wav(f"/tmp/cloned{count}.wav", SAMPLE_RATE, audio_array)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
+    temp_file_path = temp_file.name
+    write_wav(temp_file_path, SAMPLE_RATE, audio_array)
+
+    return temp_file_path
